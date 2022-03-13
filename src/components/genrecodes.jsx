a844ae6159b6112e2bafb381f7910eec73eb08cd
Abhishek Rajpoot <itsabhishekrajpoot@gmail.com>
2022-03-13 09:55:56 +0530

    save

--- src/components/genrecodes.jsx
+++ src/components/genrecodes.jsx
@@ -21,18 +21,19 @@ function GenrePage({ keyword }) {
         <div>404 Not Found</div>
       ) : (
         <>
-  
-        <div className="gpage_code_block">
-          <div className="gpage_code_block_div">{data.genre}</div>
-          <div className="gpage_code_block_ddiv"><span className="gpage_code_block_span" >code:</span> {data.code} <span className="gpage_code_block_span"><a href={"https://netflix.com/browse/genre/" + data.code}>Open on NetFlix <svg xmlns='http://www.w3.org/2000/svg'
-     stroke-width='10' stroke-dashoffset='0'
-     stroke-dasharray='0' stroke-linecap='round'
-     stroke-linejoin='round' viewBox='0 0 100 100'>
-  <polyline fill="none" points="40 20 20 20 20 90 80 90 80 60"/>
-  <polyline fill="none" points="60 10 90 10 90 40"/>
-  <line fill="none" x1="89" y1="11" x2="50" y2="50" />
-</svg></a></span></div>
+
+          <div className="gpage_code_block">
+            <div className="gpage_code_block_div">{data.genre}</div>
+            <div className="gpage_code_block_ddiv"><span className="gpage_code_block_span" >code:</span> {data.code} <span className="gpage_code_block_span"><a href={"https://netflix.com/browse/genre/" + data.code}>Open on NetFlix <svg xmlns='http://www.w3.org/2000/svg'
+              stroke-width='10' stroke-dashoffset='0'
+              stroke-dasharray='0' stroke-linecap='round'
+              stroke-linejoin='round' viewBox='0 0 100 100'>
+              <polyline fill="none" points="40 20 20 20 20 90 80 90 80 60" />
+              <polyline fill="none" points="60 10 90 10 90 40" />
+              <line fill="none" x1="89" y1="11" x2="50" y2="50" />
+            </svg></a></span></div>
           </div>
+          <div className="gpage_code_block">{data.description}</div>
         </>
       )}
     </div>
@@ -76,51 +77,51 @@ function Search() {
   const [searchTerm, setSearchTerm] = useState("");
   return (
     <>
-        <div className="search_div">
-          <svg className="svg-icon" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19.9 19.7"><g class="search-path" fill="none"><path stroke-linecap="square" d="M18.5 18.3l-5.4-5.4"/><circle cx="8" cy="8" r="7"/></g></svg>
-          <input
-            className="search_bar"
-            type="text"
-            placeholder="Search Genre code ..."
-            onChange={(e) => {
-              setSearchTerm(e.target.value);
-            }}
-          />
-        </div>
-        <h2 className="label">Code List:</h2>{" "}
+      <div className="search_div">
+        <svg className="svg-icon" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19.9 19.7"><g class="search-path" fill="none"><path stroke-linecap="square" d="M18.5 18.3l-5.4-5.4" /><circle cx="8" cy="8" r="7" /></g></svg>
+        <input
+          className="search_bar"
+          type="text"
+          placeholder="Search Genre code ..."
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+        />
+      </div>
+      <h2 className="label">Code List:</h2>{" "}
 
-        <div className="card_container" >
-          {DataList
-            .filter((array) =>
-              array.genre.toLowerCase().includes(searchTerm.toLowerCase())
-            )
-            .map((array, key) => {
-              if (searchTerm === "") {
-                return (
-                  <div className="card" key={key}>
-                    <span className="card_title">{array.genre} </span> <div className="card_div_right">
-                      <span className="card_slash">|</span> {array.code}</div>
+      <div className="card_container" >
+        {DataList
+          .filter((array) =>
+            array.genre.toLowerCase().includes(searchTerm.toLowerCase())
+          )
+          .map((array, key) => {
+            if (searchTerm === "") {
+              return (
+                <div className="card" key={key}>
+                  <span className="card_title">{array.genre} </span> <div className="card_div_right">
+                    <span className="card_slash">|</span> {array.code}</div>
+                </div>
+
+              );
+            } else {
+              return (
+                <div className="card" key={key}>
+                  <span className="card_title">{array.genre} </span>
+                  <div className="card_div_right">
+                    <span className="card_slash">|</span>
+                    <Link to={"/" + array.genre.replace(/ /g, "-").toLowerCase()}>
+                      view code
+                    </Link>
                   </div>
 
-                );
-              } else {
-                return (
-                  <div className="card" key={key}>
-                    <span className="card_title">{array.genre} </span>
-                    <div className="card_div_right">
-                      <span className="card_slash">|</span>
-                      <Link to={"/" + array.genre.replace(/ /g, "-").toLowerCase()}>
-                        view code
-                      </Link>
-                    </div>
-
-                  </div>
-                );
-              }
-            })}
-        </div>
+                </div>
+              );
+            }
+          })}
+      </div>
     </>
   );
 }
 
-export { Search, GenrePage, RelatedGenre};
+export { Search, GenrePage, RelatedGenre };
